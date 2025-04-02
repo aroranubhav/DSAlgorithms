2b686ace31c0bf6a530bec32dda80a295c5cc75a
maxi <arora.anubhav93@gmail.com>
2025-04-02 22:13:25 +0530

    Add class to handle ui state

---
+++ app/src/main/java/com/almax/dsalgorithms/presentation/base/UiState.kt
@@ -0,0 +1,10 @@
+package com.almax.dsalgorithms.presentation.base
+
+sealed interface UiState<out T> {
+
+    data class Success<T>(val data: T) : UiState<T>
+
+    data class Error(val error: String) : UiState<Nothing>
+
+    data object Loading : UiState<Nothing>
+}
