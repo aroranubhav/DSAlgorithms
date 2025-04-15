7a87538b56f1410dbbab6b29a964b600b145533b
maxi <arora.anubhav93@gmail.com>
2025-04-15 18:59:35 +0530

    Update category repository

--- app/src/main/java/com/almax/dsalgorithms/data/repository/CategoryRepositoryImpl.kt
+++ app/src/main/java/com/almax/dsalgorithms/data/repository/CategoryRepositoryImpl.kt
@@ -1,6 +1,6 @@
 package com.almax.dsalgorithms.data.repository
 
-import com.almax.dsalgorithms.data.remote.NetworkService
+import com.almax.dsalgorithms.data.remote.FoldersNetworkService
 import com.almax.dsalgorithms.domain.model.Category
 import com.almax.dsalgorithms.domain.repository.CategoryRepository
 import kotlinx.coroutines.flow.Flow
@@ -8,7 +8,7 @@ import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class CategoryRepositoryImpl @Inject constructor(
-    private val networkService: NetworkService
+    private val networkService: FoldersNetworkService
 ) : CategoryRepository {
 
     override fun getCategories(): Flow<List<Category>> {
