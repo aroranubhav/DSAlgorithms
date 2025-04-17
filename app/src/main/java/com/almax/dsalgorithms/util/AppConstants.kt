0a054789506b3a03e0b9c08e9f117eb9b1863781
maxi <arora.anubhav93@gmail.com>
2025-04-17 13:20:00 +0530

    Add constants

--- app/src/main/java/com/almax/dsalgorithms/util/AppConstants.kt
+++ app/src/main/java/com/almax/dsalgorithms/util/AppConstants.kt
@@ -4,4 +4,9 @@ object AppConstants {
 
     const val DIR_TYPE = "dir"
     const val FILE_TYPE = "file"
+    const val ALGORITHM_CATEGORY = "algorithm_category"
+    const val EASY = "Easy"
+    const val MEDIUM = "Medium"
+    const val HARD = "Hard"
+    const val UNKNOWN = "Unknown"
 }
